ef382d985f539cfffb3d580eceedf2a49086b3e9
193985 <193985@upf.br>
2024-03-21 20:55:46 -0300

    Atualização2

--- app/src/main/java/com/example/appconvidados/MainActivity.kt
+++ app/src/main/java/com/example/appconvidados/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.appconvidados
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnAdd = findViewById<FloatingActionButton>(R.id.btnAdd)
+        btnAdd.setOnClickListener{
+            val intent = Intent(this@MainActivity, guest_form::class.java)
+            startActivity(intent)
+        }
     }
 }
